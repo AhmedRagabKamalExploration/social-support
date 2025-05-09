a941b9a3dce67c60f3b319693cbf7ff13b4e851a
AhmedRagabKamalExploration <ahmed.ragab.external@exploration.space>
2025-05-09 14:04:59 +0400

    feat: expose some required components from ui-core package

--- packages/ui-core/src/index.ts
+++ packages/ui-core/src/index.ts
@@ -1,8 +1,13 @@
 // Export components
 export * from '@/components/button/button';
-export * from '@/components/typography/typography';
-export * from '@/components/dropdown-menu/dropdown-menu';
 export * from '@/components/container/container';
+export * from '@/components/dropdown-menu/dropdown-menu';
+export * from '@/components/progress/progress';
+export * from '@/components/typography/typography';
+export * from '@/components/card/card';
+
+// Export forms components [input, select, textarea, etc.]
+export type * from '@/components/forms';
 
 // Export utils
 export { cn } from '@/lib/utils';
