aa0ee72d0d077f641c48cb0fbdb117c9724e8800
AhmedRagabKamalExploration <ahmed.ragab.external@exploration.space>
2025-05-09 10:41:01 +0400

    feat: integrate ui-core inside portal next app

--- apps/portal/next.config.ts
+++ apps/portal/next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  transpilePackages: ['@dge/ui-core'],
 };
 
 export default nextConfig;
--- packages/ui-core/src/components/button/button.tsx
+++ packages/ui-core/src/components/button/button.tsx
@@ -1,48 +1,50 @@
-import { Slot } from "@radix-ui/react-slot";
-import { type VariantProps, cva } from "class-variance-authority";
-import { Loader2 } from "lucide-react";
-import * as React from "react";
+'use client';
 
-import { cn } from "@/lib/utils";
+import { Slot } from '@radix-ui/react-slot';
+import { type VariantProps, cva } from 'class-variance-authority';
+import { Loader2 } from 'lucide-react';
+import * as React from 'react';
+
+import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center dark:text-gray-50 gap-2 whitespace-nowrap rounded-md font-medium text-sm text-white disabled:text-gray-400 focus:ring-2 focus:ring-purple-400 focus:ring-offset-2  disabled:cursor-not-allowed disabled:opacity-50  ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none",
+  'inline-flex items-center justify-center dark:text-gray-50 gap-2 whitespace-nowrap rounded-md font-medium text-sm text-white disabled:text-gray-400 focus:ring-2 focus:ring-purple-400 focus:ring-offset-2  disabled:cursor-not-allowed disabled:opacity-50  ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none',
   {
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground hover:bg-primary/90 dark:bg-gray-800",
+          'bg-primary text-primary-foreground hover:bg-primary/90 dark:bg-gray-800',
         destructive:
-          "bg-destructive hover:bg-error-700 shadow-destructive-button focus:ring-error-500 disabled:bg-gray-100  disabled:border disabled:border-gray-200 disabled:shadow-disabled-button",
+          'bg-destructive hover:bg-error-700 shadow-destructive-button focus:ring-error-500 disabled:bg-gray-100  disabled:border disabled:border-gray-200 disabled:shadow-disabled-button',
         outline:
-          "border border-gray-300 bg-background hover:bg-accent text-gray-700 hover:text-accent-foreground  disabled:border-gray-200 disabled:shadow-disabled-button",
+          'border border-gray-300 bg-background hover:bg-accent text-gray-700 hover:text-accent-foreground  disabled:border-gray-200 disabled:shadow-disabled-button',
         secondary:
-          "bg-secondary text-gray-600 hover:text-gray-700 hover:bg-gray-100 disabled:shadow-disabled-button",
+          'bg-secondary text-gray-600 hover:text-gray-700 hover:bg-gray-100 disabled:shadow-disabled-button',
         ghost:
-          "hover:bg-accent text-gray-900 hover:text-accent-foreground dark:text-white",
-        link: "text-gray-600 hover:text-gray-700",
-        "link-primary": "text-purple-700 hover:text-purple-800",
+          'hover:bg-accent text-gray-900 hover:text-accent-foreground dark:text-white',
+        link: 'text-gray-600 hover:text-gray-700',
+        'link-primary': 'text-purple-700 hover:text-purple-800',
         primary:
-          "bg-purple-600 text-primary-foreground hover:bg-purple-700 shadow-button disabled:bg-gray-100  disabled:border disabled:border-gray-200 disabled:shadow-disabled-button",
-        rounded: "bg-purple-600 hover:bg-purple-700 shadow-button rounded-full",
+          'bg-purple-600 text-primary-foreground hover:bg-purple-700 shadow-button disabled:bg-gray-100  disabled:border disabled:border-gray-200 disabled:shadow-disabled-button',
+        rounded: 'bg-purple-600 hover:bg-purple-700 shadow-button rounded-full',
       },
       size: {
-        sm: "h-9 rounded-md py-2 px-3 text-sm",
-        md: "h-10 py-2.5 px-3.5 text-base",
-        lg: "h-11 rounded-md py-2.5 px-4 text-base",
-        xl: "py-3 px-4.5 h-12 text-base",
-        "2xl": "py-4 px-5.5 h-14 text-xl",
-        icon: "size-8 rounded-full hover:bg-primary/10 shrink-0",
+        sm: 'h-9 rounded-md py-2 px-3 text-sm',
+        md: 'h-10 py-2.5 px-3.5 text-base',
+        lg: 'h-11 rounded-md py-2.5 px-4 text-base',
+        xl: 'py-3 px-4.5 h-12 text-base',
+        '2xl': 'py-4 px-5.5 h-14 text-xl',
+        icon: 'size-8 rounded-full hover:bg-primary/10 shrink-0',
       },
       iconPosition: {
-        before: "flex-row",
-        after: "flex-row-reverse",
+        before: 'flex-row',
+        after: 'flex-row-reverse',
       },
     },
     defaultVariants: {
-      variant: "default",
-      size: "md",
-      iconPosition: "after",
+      variant: 'default',
+      size: 'md',
+      iconPosition: 'after',
     },
   },
 );
@@ -71,7 +73,7 @@ function Button({
   ...props
 }: ButtonProps) {
   // eslint-disable-next-line @typescript-eslint/naming-convention
-  const Comp = asChild ? Slot : "button";
+  const Comp = asChild ? Slot : 'button';
 
   return (
     <Comp
@@ -95,7 +97,7 @@ function Button({
   );
 }
 
-Button.displayName = "Button";
+Button.displayName = 'Button';
 
 // eslint-disable-next-line react-refresh/only-export-components
 export { Button, buttonVariants };
